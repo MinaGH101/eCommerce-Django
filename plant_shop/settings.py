c5ae36e4026c908081ad6391ab7d343157e7ecd6
minagholami <migholami101@gmail.com>
2023-05-22 21:04:24 +0430

    heroku push

--- plant_shop/settings.py
+++ plant_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,7 @@ MESSAGE_TAGS = {
     messages.WARNING: "warning",
     messages.ERROR: "error",
 }
+
+
+
+django_heroku.settings(locals())
